0404c6a31cca6cd92cb60ea8611d80ff6ee0d6e0
SheharyarTariq <sheharyartariqbutt@gmail.com>
2026-06-28 16:33:48 +0500

    feat: change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,29 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Laundry Free",
   description: "Laundry Free Admin Panel",
+  icons: {
+    icon: [
+      { url: "/assets/favicon_io/favicon.ico", sizes: "any" },
+      {
+        url: "/assets/favicon_io/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/assets/favicon_io/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/assets/favicon_io/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/assets/favicon_io/site.webmanifest",
   openGraph: {
     title: "Laundry Free",
     description: "Laundry Free Admin Panel",
